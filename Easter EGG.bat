2a291ab3db21c13f9dd6afb99cf313aa20630dba
Xenuzz <122729688+Xenuzz@users.noreply.github.com>
2024-01-22 16:50:18 +0100

    Update Easter EGG.bat

--- Easter EGG.bat
+++ Easter EGG.bat
@@ -2,17 +2,17 @@
 echo Welcome to the Easter EGG!
 echo Now You will see the Easter EGG
 
-rem Warte für 2 Sekunden
+rem 
 timeout /nobreak /t 2 >nul
 
 echo Bereit für eine Überraschung?
 
-rem Zeige ein ASCII-Art-Smile
+rem 
 echo.
 echo   :-)
 echo.
 
-rem Warte für 3 Sekunden
+rem 
 timeout /nobreak /t 3 >nul
 
 echo Spaß beendet! Auf Wiedersehen!
